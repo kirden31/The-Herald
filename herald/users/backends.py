45519511a8a1aaf92460a4b20353faaeb448d6d7
Ornella Sultanbekova <odsultanbekova@edu.hse.ru>
2025-12-20 14:21:49 +0300

    feat: added activate and deactivate for account

--- herald/users/backends.py
+++ herald/users/backends.py
@@ -27,19 +27,24 @@ class ConfigAuthBackend(django.contrib.auth.backends.ModelBackend):
         if user.check_password(password) and self.user_can_authenticate(
             user=user,
         ):
-            user.profile.attempts_count = 0
-            user.profile.save()
+            profile = user.profile
+            profile.attempts_count = 0
+            profile.blocked_at = None
+            profile.save()
             return user
 
-        user.profile.attempts_count += 1
+        profile = user.profile
+        profile.attempts_count += 1
 
-        if user.profile.attempts_count > django.conf.settings.MAX_AUTH_ATTEMPTS:
+        if profile.attempts_count > django.conf.settings.MAX_AUTH_ATTEMPTS:
             user.is_active = False
-            user.profile.block_date = django.utils.timezone.now()
+            profile.blocked_at = django.utils.timezone.now()
+            user.save()
+            profile.save()
 
             if request:
                 activation_url = request.build_absolute_uri(
-                    django.urls.reverse('users:reactivate', kwargs={'pk': user.id}),
+                    django.urls.reverse('users:account_reactivate', kwargs={'pk': user.id}),
                 )
 
                 msg = gettext_lazy(
@@ -55,7 +60,6 @@ class ConfigAuthBackend(django.contrib.auth.backends.ModelBackend):
                     fail_silently=False,
                 )
 
-        user.save()
         return None
 
     def get_user(self, user_id):
--- herald/users/models.py
+++ herald/users/models.py
@@ -99,6 +99,12 @@ class Profile(django.db.models.Model):
         verbose_name=_('favorite categories'),
         help_text=_('Favorite categories'),
     )
+    blocked_at = django.db.models.DateTimeField(
+        verbose_name=_('deactivate in'),
+        help_text=_('account was deactivated'),
+        null=True,
+        editable=False,
+    )
 
     def get_image_300x300(self):
         if self.image:
